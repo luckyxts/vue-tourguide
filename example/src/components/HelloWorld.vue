eb5abd0d7b8178a02836d499f77e5105ddf99756
luckyxts <330995827@qq.com>
2020-06-26 14:18:49 +0800

    文档

--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -28,8 +28,7 @@
             <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
             <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
         </ul>
-        <button @click="showMethods">123</button>
-        {{show}}
+        <button @click="showMethods">show tour</button>
         <tourGuide :tourConfigList="tourConfigList" :show="show" @closeTour="closeTour"></tourGuide>
     </div>
 </template>
